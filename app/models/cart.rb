cd4275018a7ab1488c9a0af8880125cebdb7837e
Joseph Hung <arixxle@gmail.com>
2020-04-14 14:24:01 +0800

    基本功能３－５

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,13 +4,32 @@ class Cart
   end
   
   def add_item(item_id)
-    @items << item_id
+    found_item = @items.find { |item| item.item_id == item_id }
+
+    if found_item
+      found_item.increment!
+    else
+      @items << CartItem.new(item_id)
+    end
+
   end
+
   def empty?
     @items.empty?
   end
 
   def items
     @items
+  end
+
+  def total
+    @items.reduce(0) { |sum, item| sum + item.total }
+
+    # @items.sum { |item| item.total}
+
+    # tmp = 0
+    # @items.each do |item|
+    #   tmp += item.total
+    # end
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -15,10 +15,34 @@ RSpec.describe Cart, type: :model do
       3.times { cart.add_item(1) }
       2.times { cart.add_item(2) }
       2.times { cart.add_item(1) }
-      TODO:
       expect(cart.items.count).to be 2
-      
     end
+
+    it "商品可以放到購物車裡，也可以再拿出來。" do
+      cart = Cart.new
+
+      # cat1 = FactoryBot.create(:category)
+      i1 = FactoryBot.create(:item)
+      i2 = FactoryBot.create(:item)
+      
+      3.times { cart.add_item(i1.id) }
+      2.times { cart.add_item(i2.id) }
+
+      expect(cart.items.first.item).to be_an Item 
+      expect(cart.items.first.item.price).to be i1.price
+    end
+
+    it "可以計算整台購物車的總消費金額。" do
+      cart = Cart.new
+      i1 = FactoryBot.create(:item, price: 50)
+      i2 = FactoryBot.create(:item, price: 100)
+
+      3.times { cart.add_item(i1.id)}
+      2.times { cart.add_item(i2.id)}
+
+      expect(cart.total).to be 350
+    end
+
   end
   describe "進階功能" do
   end
