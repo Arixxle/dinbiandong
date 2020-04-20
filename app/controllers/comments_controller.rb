daf3167ed2b7e2954b18a2a8766545f5650fbf42
Joseph Hung <arixxle@gmail.com>
2020-04-20 16:09:30 +0800

    add AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    #寫法1
-    # @comment = Comment.new(comment_params)
-    # @comment.item_id = params[:item_id] 
-    # @comment.user_id = current_user.id
-
-    #寫法2
-  #   @comment = Comment.new(comment_params,
-                            # item_id: params[:item_id],
-                            #  user_id: current_user.id )
-  # @comment = current_user.comments.build(comment_params, user: current_user)  
-
-    #寫法3
     @item = Item.find(params[:item_id])
-    # @comment = current_user.comments.build(comment_params, item_id: @item.id)
-    @comment = @item.comments.build(comment_params.merge(user: current_user)) #同上效果因為有belongs_to: item
-    # @comment = current_user.comment.build(comment_params.merge(item: @item))
-    # @comment.item = @item
+    @comment = @item.comments.build(comment_params.merge(user: current_user)) 
     
-
     if @comment.save
-      # redirect_to root_path, notice: 'OK'
-      #轉到首頁，出現提示
-      # redirect_to item_path(params[:item_id]), notice: 'Cool'
-      #轉回同一個頁面出現flash
 
-      #AJAX
-      # render js: "alert('hi')"
-      # render json: {name: '111', age: 18} #進console才看得到
 
     else
       # redirect_to item_path(params[:item_id]), notice: 'No good!'
