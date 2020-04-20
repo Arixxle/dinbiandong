b941fb9a514ccc600205197f4d72ae92fc40b820
Joseph Hung <arixxle@gmail.com>
2020-04-20 17:15:56 +0800

    add stimulus

---
+++ app/javascript/controllers/item_controller.js
@@ -0,0 +1,39 @@
+// Visit The Stimulus Handbook for more details 
+// https://stimulusjs.org/handbook/introduction
+// 
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  // static targets = [ "output" ]
+  static targets = [ "icon" ]//選到html el 
+
+  initialize(){
+    this.clicked = false;
+  }
+
+  heart(e){
+    e.preventDefault()
+    console.log('Heart is clicked!')
+    if (this.clicked){
+      this.iconTarget.classList.remove('fas');
+      this.iconTarget.classList.add('far');
+      this.clicked = false;
+    } else {
+      this.iconTarget.classList.remove('far');
+      this.iconTarget.classList.add('fas');
+      this.clicked = true;
+    }
+
+  }
+
+  connect() {
+    // this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log('HI123')
+  }
+}
